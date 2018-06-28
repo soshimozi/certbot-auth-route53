b6741992dcfc6d377f60c3075c8917e4808286c5
Scott McCain <scott.mccain@gmail.com>
2018-06-28 15:24:44 -0700

    Simplified auth script

--- bin/certbot-auth.sh
+++ bin/certbot-auth.sh
@@ -38,51 +38,71 @@ CERT_DIR=${FOLDER:-"${PWD}/letsencrypt"}
 CHECK_DIR="${CERT_DIR}/live/${DOMAIN}-0001"
 OPTIONAL=$1
 
-echo "CHECK_DIR ${CHECK_DIR}"
+#echo "CHECK_DIR ${CHECK_DIR}"
+#
+#firstrun() {
+#    mkdir -p "${CERT_DIR}"
+#
+#    echo "Attempting to create certificates"
+#
+#    certbot certonly \
+#     --non-interactive \
+#     --manual \
+#     --manual-auth-hook "${PWD}/auth-hook.js" \
+#     --manual-cleanup-hook "${PWD}/auth-hook.js" \
+#     --preferred-challenge dns \
+#     --config-dir "${CERT_DIR}" \
+#     --work-dir "${CERT_DIR}" \
+#     --logs-dir "${CERT_DIR}" \
+#     --agree-tos \
+#     --email "${EMAIL}" \
+#     --manual-public-ip-logging-ok \
+#     --deploy-hook "${PWD}/deploy-hook.js" \
+#     -d "${DOMAIN}" \
+#     $STAGING \
+#     $OPTIONAL \
+#     "$@"
+#}
 
-firstrun() {
-    mkdir -p "${CERT_DIR}"
+#renew() {
+#    echo "Attempting to renew existing certificates"
+#
+#    certbot renew \
+#     --config-dir "${CERT_DIR}" \
+#     --work-dir "${CERT_DIR}" \
+#     --logs-dir "${CERT_DIR}" \
+#     --deploy-hook "${PWD}/deploy-hook.js" \
+#     $STAGING \
+#     $OPTIONAL \
+#     "$@"
+#}
 
-    echo "Attempting to create certificates"
+mkdir -p "${CERT_DIR}"
 
-    certbot certonly \
-     --non-interactive \
-     --manual \
-     --manual-auth-hook "${PWD}/auth-hook.js" \
-     --manual-cleanup-hook "${PWD}/auth-hook.js" \
-     --preferred-challenge dns \
-     --config-dir "${CERT_DIR}" \
-     --work-dir "${CERT_DIR}" \
-     --logs-dir "${CERT_DIR}" \
-     --agree-tos \
-     --email "${EMAIL}" \
-     --manual-public-ip-logging-ok \
-     --deploy-hook "${PWD}/deploy-hook.js" \
-     -d "${DOMAIN}" \
-     $STAGING \
-     $OPTIONAL \
-     "$@"
-}
+certbot certonly \
+ --non-interactive \
+ --manual \
+ --manual-auth-hook "${PWD}/auth-hook.js" \
+ --manual-cleanup-hook "${PWD}/auth-hook.js" \
+ --preferred-challenge dns \
+ --config-dir "${CERT_DIR}" \
+ --work-dir "${CERT_DIR}" \
+ --logs-dir "${CERT_DIR}" \
+ --agree-tos \
+ --email "${EMAIL}" \
+ --manual-public-ip-logging-ok \
+ --deploy-hook "${PWD}/deploy-hook.js" \
+ -d "${DOMAIN}" \
+ $STAGING \
+ $OPTIONAL \
+ "$@"
 
-renew() {
-    echo "Attempting to renew existing certificates"
-
-    certbot renew \
-     --config-dir "${CERT_DIR}" \
-     --work-dir "${CERT_DIR}" \
-     --logs-dir "${CERT_DIR}" \
-     --deploy-hook "${PWD}/deploy-hook.js" \
-     $STAGING \
-     $OPTIONAL \
-     "$@"
-}
-
-if [[ -n $1 ]]; then
- echo "optional parameter: $1"
-fi
-
-if [[ -d "${CHECK_DIR}" && ! -L "${CHECK_DIR}" ]] ; then
-    renew
-else
-    firstrun
-fi
+#if [[ -n $1 ]]; then
+# echo "optional parameter: $1"
+#fi
+#
+#if [[ -d "${CHECK_DIR}" && ! -L "${CHECK_DIR}" ]] ; then
+#    renew
+#else
+#    firstrun
+#fi
