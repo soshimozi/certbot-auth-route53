86e183faf4d7f65859fe9236338213af43500bba
Scott McCain <scott.mccain@gmail.com>
2018-06-30 01:32:22 -0700

    Fixed s3upload

--- s3-deploy.js
+++ s3-deploy.js
@@ -1,10 +1,35 @@
-module.exports = (kms) => {
+module.exports = (kms, region) => {
 
     const uploadS3WithEnvelope = async (bucket, key, cmkId, plainText, encoding ='utf8') => {
 
-        const { createTenantMasterKey, encryptEnvelope } = require('./envelope-encryption');
-        let tmk = await createTenantMasterKey(cmkId);
-        return encryptEnvelope(cmkId, tmk.cipherText)(plainText, encoding);
+        const { S3 } = require('aws-sdk');
+        const s3 = new S3({region});
+
+        const { createTenantMasterKey, encryptEnvelope } = require('./envelope-encryption')(kms);
+   
+        let tmk = await createTenantMasterKey(cmkId)
+
+        let envelope = await encryptEnvelope(cmkId, tmk.cipherText)(plainText, encoding);
+        
+        let params = {
+            Body: envelope.dataCipherText,
+            Bucket: bucket,
+            Key: key,
+            ServerSideEncryption: 'AES256',
+            Metadata: {
+                'tdkCipherText': envelope.tdkCipherText,
+                'tmkCipherText': envelope.tmkCipherText
+            }
+        }
+        
+        return new Promise((resolve,reject) => {
+    
+            s3.putObject(params, function(err, data) {
+                if(err) reject(err);
+                else resolve(data);
+            });
+        });        
+
     };
 
     const downloadS3Envelope = async (bucket, key, outputEnc='base64') => {
